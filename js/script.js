8231c78b12d1d26ea9a6d8113094d3895339d010
wilhelmGreene <greenewilhelm@gmail.com>
2015-09-26 09:44:54 +0300

    Final changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function show()
+{
+	var x = document.getElementById("hide-show");
+	x.style.visibility = "visible";
+}
+function hide()
+{
+	var x = document.getElementById("hide-show");
+	x.style.visibility = "collapse";
+}
+function editProfileDriver() 
+{
+	var x = document.getElementById("saveChanges");
+	x.style.visibility = "visible";
+}
